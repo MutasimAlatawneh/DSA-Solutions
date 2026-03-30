6b4f9b2a9192579527d87aabee9875e656e91e0f
motasem alatawna <129550250+MutasimAlatawneh@users.noreply.github.com>
2026-03-30 21:36:15 +0300

    Add: two-integer-sum-ii - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-4.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        vector<int>ans;
+        bool fl=false;
+        for(int i=0;i<numbers.size();i++){
+            for(int j=i+1;j<numbers.size();j++){
+                if(numbers[i]+numbers[j]==target)
+                {
+                    ans.push_back(i+1);
+                    ans.push_back(j+1);
+                    fl=true;
+                    break;
+                }
+                
+            }
+            if(fl)break;
+        }
+        return ans;
+    }
+};
