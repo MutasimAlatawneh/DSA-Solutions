1a6f232678fd52076fc211b0db8d49888e4b8412
motasem alatawna <129550250+MutasimAlatawneh@users.noreply.github.com>
2026-04-30 14:56:35 +0300

    Add: merge-strings-alternately - submission-1

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string ans="";
+        int n=word1.size();
+        int a=0,b=0;
+        while(a<word1.size()||b<word2.size()){
+            if(a<word1.size())
+            ans+=word1[a++];
+            if(b<word2.size())
+            ans+=word2[b++];
+        }
+        return ans;
+    }
+};
